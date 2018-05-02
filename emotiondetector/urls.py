11f89bce7831d6fd5cf62240b468d0643a01ab9a
Hossam <hossam.hosni.95@gmail.com>
2018-05-02 21:59:00 +0200

    Add Webcam support

--- emotiondetector/urls.py
+++ emotiondetector/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.detect, name='detect')
+    path('', views.detect, name='detect'),
+    path('webcam', views.webcam, name='webcam')
 ]
--- emotiondetector/views.py
+++ emotiondetector/views.py
@@ -2,12 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+from django import forms
 from . import recognizer
 import cv2
 import glob
 # Create your views here.
 
 def index(request):
+    return render(request, 'emotiondetector/home.html')
+
+def webcam(request):
+    cap = cv2.VideoCapture(0)
+
+    while(True):
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite("testing image.jpg", gray)
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
     return render(request, 'emotiondetector/home.html')
 
 def detect(request):
